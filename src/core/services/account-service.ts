b47f44b145f3e3676b2a7bb5907f406130251868
olaoluwapaul <adesolapaul@gmail.com>
2026-06-30 21:44:25 +0200

    Persisting login using signals.

--- src/core/services/account-service.ts
+++ src/core/services/account-service.ts
@@ -1,13 +1,28 @@
-import { Service, inject } from '@angular/core';
+import { Service, inject, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { User } from '../../types/user';
+import { tap } from 'rxjs/operators';
 
 @Service()
 export class AccountService {
     private http = inject(HttpClient);
+    currentUser = signal<User | null>(null);
 
     baseUrl = 'https://localhost:5001/api/';
 
     login(creds: any){
-        return this.http.post(this.baseUrl + 'account/login', creds);
+        return this.http.post<User>(this.baseUrl + 'account/login', creds).pipe(
+            tap(user => {
+                if (user) {
+                    localStorage.setItem('user', JSON.stringify(user));
+                    this.currentUser.set(user);
+                }
+            })
+        )
+    }
+
+    logout() {
+        localStorage.removeItem('user');
+        this.currentUser.set(null);
     }
 }
